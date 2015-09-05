f6c14387d3db115d9897764ca723ca9d993f3996
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-05 11:41:02 -0400

    Add babel task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require("gulp"),
-  connect = require("gulp-connect");
+  connect = require("gulp-connect"),
+  babel = require("gulp-babel");
 
 gulp.task("webserver", function () {
   connect.server();
+});
+
+gulp.task("babel", function () {
+  return gulp.src("src/js/bpm-counter.js")
+  .pipe(babel())
+  .pipe(gulp.dest("dist/"));
 });
 
 gulp.task("default", ["webserver"]);
