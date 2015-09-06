c1a8fc7e47a73b060383ce9e6d642dc65f5c21c5
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-05 22:08:52 -0400

    Update BPM

--- src/js/bpm-counter.js
+++ src/js/bpm-counter.js
@@ -1,10 +1,24 @@
 class BpmCounter {
+  constructor() {
+    this.numTaps = 0;
+    this.bpm = 0;
+  }
+
   start() {
     console.log("Starting timer");
+    this.numTaps = 1;
+
+    this.lastTappedTime = new Date();
   }
 
   tick() {
     console.log("Tick!");
-  }
+    this.numTaps++;
 
+    let currentTime = new Date();
+
+    let timeDifferenceInMS = currentTime - this.lastTappedTime;
+
+    this.bpm = this.numTaps / (timeDifferenceInMS / 1000);
+  }
 }
