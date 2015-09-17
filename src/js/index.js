79157955bdcbb57577b562edd5b1122431fb6570
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-16 18:44:51 -0700

    Handle keydown

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   let bpmCounter = new BpmCounter();
+
+  $(document.body).on("keydown", handleKeyDown);
 });
+
+function handleKeyDown () {
+  console.log("Key down");
+}
