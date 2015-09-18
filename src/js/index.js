d5f6a50ceda6f30bfada9a39e5438976abc93ac1
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-17 18:58:56 -0700

    Update bpm info on tap

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
   let bpmCounter = new BpmCounter();
 
-  $(document.body).on("keydown", handleKeyDown);
-});
+  $(document.body).on("keydown", function () {
+    console.log("Keydown!");
+    bpmCounter.tick();
 
-function handleKeyDown () {
-  console.log("Key down");
-}
+    console.log(bpmCounter);
+
+    console.log(bpmTable);
+    bpmTable.setProps({
+      bpm_info: {
+        bpm: bpmCounter.bpm,
+        timingTaps: bpmCounter.numTaps,
+      },
+    });
+  });
+});
