5a6bf4ef77a0005fb2ea05f7616d5d99ab9df10f
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-18 23:49:39 -0700

    Fix bug where the BPM calculated was wrong

--- src/js/bpm-counter.js
+++ src/js/bpm-counter.js
@@ -2,22 +2,25 @@ class BpmCounter {
   constructor() {
     this.numTaps = 0;
     this.bpm = 0;
-    this.lastTappedTime = new Date();
+    this.startTapTime = new Date();
   }
 
   restart() {
     this.numTaps = 0;
     this.bpm = 0;
-    this.lastTappedTime = new Date();
   }
 
   tick() {
+    if (this.numTaps === 0) {
+      this.startTapTime = new Date();
+    }
+
     this.numTaps++;
 
     let currentTime = new Date();
 
-    let timeDifferenceInMS = currentTime - this.lastTappedTime;
+    let timeDifferenceInMS = currentTime - this.startTapTime;
 
-    this.bpm = this.numTaps / (timeDifferenceInMS / 1000);
+    this.bpm = this.numTaps / (timeDifferenceInMS / 1000) * 60;
   }
 }
