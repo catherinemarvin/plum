8cb06994baef7b9fdc8b69702bfcd09e4e64f784
Kevin Hwang <k3vinhwang@gmail.com>
2015-09-20 14:34:06 -0700

    Remove unused console.log

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,7 @@ $(document).ready(function () {
   let bpmCounter = new BpmCounter();
 
   $(document.body).on("keydown", function () {
-    console.log("Keydown!");
     bpmCounter.tick();
-
-    console.log(bpmCounter);
-
-    console.log(bpmTable);
     bpmTable.setProps({
       bpm_info: {
         bpm: bpmCounter.bpm,
